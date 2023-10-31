b217db4da3809ac0258bb3b1e4fe73db6e0e627b
Henry Tena <henry.tena@ffufm.com>
2023-10-31 13:52:56 +0800

    add path

--- lib/class/sqflite_handler.dart
+++ lib/class/sqflite_handler.dart
@@ -1,45 +1,65 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class AnimalDatabaseHandler {
-  static const _dbPath = 'animals.db';
   static const _tableName = 'animals';
   static const _columnId = 'id';
   static const _columnTitle = 'name';
 
   late Database db;
 
-  Future<Database> open() async {
-    db = await openDatabase(_dbPath, version: 1, onCreate: (Database db, int version) async {
-      await db.execute('''
-                      create table $_tableName ( 
-                        $_columnId integer primary key autoincrement, 
-                        $_columnTitle text not null)
-                      ''');
-    });
-
-    return db;
+  Future<void> open() async {
+    db = await openDatabase(
+      join(await getDatabasesPath(), 'animals.db'),
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute('''
+          CREATE TABLE $_tableName (
+            $_columnId INTEGER PRIMARY KEY,
+            $_columnTitle TEXT
+          )
+        ''');
+      },
+    );
   }
 
-  Future<void> insert(String animalName) async => db.insert(_tableName, {_columnId: null, _columnTitle: animalName});
+  Future<void> insert(String animalName) async {
+    final data = <String, dynamic>{_columnTitle: animalName};
+    await db.insert(_tableName, data);
+  }
 
-  Future<List<Map<dynamic, dynamic>>> getAnimals(int? id) async {
+  Future<List<Map<String, dynamic>>> getAnimals(int? id) async {
     if (id != null) {
-      List<Map> maps = await db.query(_tableName, columns: [_columnId], where: '$_columnId = ?', whereArgs: [id]);
+      final maps = await db.query(_tableName,
+          columns: [_columnId, _columnTitle],
+          where: '$_columnId = ?',
+          whereArgs: [id]);
 
-      return maps.isNotEmpty ? maps : List.empty();
+      return maps.isNotEmpty ? maps : [];
+    } else {
+      final maps = await db.query(_tableName,
+          columns: [_columnId, _columnTitle]);
+
+      return maps;
     }
-
-    List<Map> maps = await db.query(_tableName, columns: [_columnId, _columnTitle]);
-
-    return maps;
   }
 
   Future<int> delete(int id) async {
-    return await db.delete(_tableName, where: '$_columnId = ?', whereArgs: [id]);
+    return await db.delete(
+      _tableName,
+      where: '$_columnId = ?',
+      whereArgs: [id],
+    );
   }
 
-  Future<int> update(int animalId) async {
-    return await db.update(_tableName, {_columnId: animalId}, where: '$_columnId = ?', whereArgs: [animalId]);
+  Future<int> update(int animalId, String animalName) async {
+    final data = <String, dynamic>{_columnTitle: animalName};
+    return await db.update(
+      _tableName,
+      data,
+      where: '$_columnId = ?',
+      whereArgs: [animalId],
+    );
   }
 
   Future close() async => db.close();
